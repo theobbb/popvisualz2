aaa95e6150eed6fe7a51c431bcf2e2950158acc4
Theo <theobbb64@gmail.com>
2025-10-11 13:54:42 -0400

    s

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,8 +7,22 @@
 	});
 </script>
 
-<div class="fixed inset-0">
-	<mux-player
+<div class="fixed inset-0 -z-1">
+	<video
+		autoplay
+		muted
+		loop
+		playsinline
+		preload="auto"
+		class={[
+			'absolute inset-0 h-full w-full object-cover opacity-0',
+			mounted && 'opacity-100',
+			'transition'
+		]}
+	>
+		<source src="/videos/reel-small-2.mp4" type="video/mp4" />
+	</video>
+	<!-- <mux-player
 		playback-id="qRls02Bvl9Piha6rp99a82jaRiJx015hZbqSpHF02vLkfU"
 		autoplay
 		muted
@@ -17,5 +31,5 @@
 		stream-type="on-demand"
 		max-resolution="1080p"
 		style="position: fixed; top: 0; left: 0; min-width: 100vw; min-height: 100vh; object-fit: cover; z-index: -1;"
-	></mux-player>
+	></mux-player> -->
 </div>
