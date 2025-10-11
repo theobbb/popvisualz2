50ff193461db2f6754805cbc4249e29d81aa0b10
Theo <theobbb64@gmail.com>
2025-10-11 13:05:51 -0400

    first

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let mounted = $state(false);
+	onMount(() => {
+		mounted = true;
+	});
+</script>
+
+<div class="fixed inset-0">
+	<mux-player
+		playback-id="qRls02Bvl9Piha6rp99a82jaRiJx015hZbqSpHF02vLkfU"
+		autoplay
+		muted
+		loop
+		playsinline
+		stream-type="on-demand"
+		max-resolution="1080p"
+		style="position: fixed; top: 0; left: 0; min-width: 100vw; min-height: 100vh; object-fit: cover; z-index: -1;"
+	></mux-player>
+</div>
---
+++ src/routes/header.svelte
@@ -0,0 +1,19 @@
+<header class="grid-8 leading-5">
+	<div class="text-right">Directed by</div>
+	<div>Pop Visualz</div>
+	<div class="col-span-2"></div>
+
+	<a href="/featured-projects" class="hover:bg-white hover:text-black">
+		Featured <br /> Projects
+	</a>
+
+	<a href="/featured-projects" class="hover:bg-white hover:text-black">
+		Social Media <br /> Content
+	</a>
+
+	<a href="/featured-projects" class="hover:bg-white hover:text-black">
+		The Writer <br /> (short film)
+	</a>
+
+	<a href="/featured-projects" class="hover:bg-white hover:text-black"> Contact </a>
+</header>
