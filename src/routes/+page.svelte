111b82873f5eb86a95ca4add7350cc8564cab2fa
Theo <theobbb64@gmail.com>
2026-01-15 19:20:12 -0500

    g

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+	import IconArrow from '$lib/icons/icon-arrow.svelte';
 	import Reel from './reel.svelte';
-	import TrustedBy from './trusted-by.svelte';
+
+	let mouse: { x: number; y: number } = $state({ x: -1000, y: -1000 });
+
+	function onmousemove(event: MouseEvent) {
+		mouse = { x: event.clientX, y: event.clientY };
+	}
+
+	$effect(() => {
+		window.addEventListener('mousemove', onmousemove);
+		return () => {
+			window.removeEventListener('mousemove', onmousemove);
+		};
+	});
 </script>
 
 <svelte:head>
@@ -21,5 +35,17 @@
 		class="rotate-12- absolute bottom-0 left-0 h-96 w-full bg-gradient-to-b from-transparent to-black/90"
 	></div>
 </div>
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<!-- svelte-ignore a11y_no_static_element_interactions -->
+<div onclick={() => goto('/featured-projects')} class="fixed inset-0 cursor-pointer"></div>
 
+<div
+	class="pointer-events-none fixed top-0 left-6 mix-blend-difference"
+	style="transform: translate({mouse.x}px, {mouse.y}px);"
+>
+	<div class="flex gap-1">
+		<div class="mt-0.5"><IconArrow /></div>
+		<div>Featured<br /> Projects</div>
+	</div>
+</div>
 <!-- <TrustedBy /> -->
